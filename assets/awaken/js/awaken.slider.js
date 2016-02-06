2baa667c13d27fdb3cc025a8f0551f82c89c65cd
arhen <official.rahmatslamet@gmail.com>
2016-02-06 14:04:43 +0800

    hapus file2 yang tidak perlu

--- assets/awaken/js/awaken.slider.js
+++
@@ -1,8 +0,0 @@
-jQuery(window).load(function() {
-  jQuery('.flexslider').flexslider({
-    animation: "fade",
-    direction: "horizontal",
-	slideshowSpeed: 6000,
-	animationSpeed: 600,
-  });
-});
